8ee90d2e70e7fdb5754e40138150f30227d7772b
E Flynn <erflynn@stanford.edu>
2019-03-20 11:21:58 -0700

    Moved example code to a vignette, fixed a visualization bug.

--- R/visualizeXYplot.R
+++ R/visualizeXYplot.R
@@ -8,6 +8,7 @@
 #' @return a graph of RPS4Y1 vs. XIST probes corresponding to the sex labels of studies
 
 visualizeXYplot <- function (expr_matrix, keys, sex_lab, x_axis = "XIST", y_axis = "RPS4Y1") {
+  require('dplyr')
 
   keys <- as.data.frame(keys)
   keys$probe <- rownames(keys)
@@ -36,10 +37,13 @@ visualizeXYplot <- function (expr_matrix, keys, sex_lab, x_axis = "XIST", y_axis
     return("Error: RPS4Y1 gene missing.")
   }
 
+  probe_map <- unlist(c(rep("XIST", length(xist.probes)), rep("RPS4Y1", length(rps4y1.probes))))
+  names(probe_map) <- unlist(c(xist.probes, rps4y1.probes))
   #   plot the ranked XIST and RPS4Y1 data
+  rank_data <- convertToGenes(expr_matrix, probe_map, c("XIST", "RPS4Y1"))
   xy_vals <- rank_data[c('XIST', 'RPS4Y1'),]
   sex_lab <- as.data.frame(sex_lab)
   names(sex_lab) <- c("sex_lab")
   my.colors <- sapply(sex_lab$sex_lab, function(x) { ifelse(x == 1, "blue", "red")})
-  plot(xy_vals['XIST',], xy_vals['RPS4Y1',], xlab = 'XIST', ylab = "RPS4Y1", col=my.colors)
+  plot(t(xy_vals), xlab = 'XIST', ylab = "RPS4Y1", col=my.colors)
 }
